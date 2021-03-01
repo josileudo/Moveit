3dc7f022d0259ff153f9c39f88f2c4580a3594e9
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2021-03-01 16:56:54 -0300

    adicionando favicon e titulo de inicio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import {Profile} from "../components/Profile"
 import {ExperienceBar} from "../components/ExperienceBar"
 
+import Head from "next/head"
+
 import styles from "../styles/pages/Home.module.css"
 import { CompletedChallenges } from "../components/CompletedChallenges"
 import { Countdown } from "../components/Countdown"
@@ -8,6 +10,9 @@ import { Countdown } from "../components/Countdown"
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title> Início | Move.it</title>
+      </Head>
       <ExperienceBar/>
       <section className= {styles.leftContainer}>
         <div>
